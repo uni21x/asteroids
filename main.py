f6dc823ac8adc59f78ebb58e6055cc94b3d28bab
uni21x <unilaw@gmx.de>
2025-09-01 20:45:20 +0200

    delta time / circleshape.py

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ def main():
 Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}""")
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
+
         
-
-    
-
-
 if __name__ == "__main__":
     main()
