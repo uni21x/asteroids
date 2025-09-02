2b54ef086a807cb0ad1e9e02c6c4ce53545203c5
uni21x <unilaw@gmx.de>
2025-09-02 19:48:54 +0200

    draw player

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ Screen height: {SCREEN_HEIGHT}""")
     
     clock = pygame.time.Clock()
     dt = 0
-    
+
+    from player import Player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Draw player
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
