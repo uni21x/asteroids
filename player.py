485fa220591fc3b27bc0ceeb348b4d9858862c79
uni21x <unilaw@gmx.de>
2025-09-02 20:02:39 +0200

    moving/draw vector

--- player.py
+++ player.py
@@ -27,10 +27,19 @@ class Player(CircleShape):
         from constants import PLAYER_TURN_SPEED
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        from constants import PLAYER_SPEED
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(-dt)  # left
+            self.rotate(-dt)  
         if keys[pygame.K_d]:
-            self.rotate(dt)   # right
+            self.rotate(dt)   
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
