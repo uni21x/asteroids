4be61d250652ed00c3404c3f6ee76e381f34d353
uni21x <unilaw@gmx.de>
2025-09-02 20:08:21 +0200

    creating groups and using groups

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ Screen height: {SCREEN_HEIGHT}""")
     dt = 0
 
     from player import Player
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,8 +22,9 @@ Screen height: {SCREEN_HEIGHT}""")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
